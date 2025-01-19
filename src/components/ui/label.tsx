92b53b091ef1975e58ee4fa307cd4a0e928e74dc
Brian Mangi <brian@itsup.com.sb>
2025-01-19 18:14:05 +1100

    bug fix 7

--- src/components/ui/label.tsx
+++ src/components/ui/label.tsx
@@ -3,8 +3,7 @@
 import * as React from "react"
 import * as LabelPrimitive from "@radix-ui/react-label"
 import { cva, type VariantProps } from "class-variance-authority"
-
-import { mergeStyles } from "@/lib/style-merge"
+import { cn } from "@/lib/utils"
 
 const labelVariants = cva(
   "text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
@@ -17,7 +16,7 @@ const Label = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <LabelPrimitive.Root
     ref={ref}
-    className={mergeStyles(labelVariants(), className)}
+    className={cn(labelVariants(), className)}
     {...props}
   />
 ))
